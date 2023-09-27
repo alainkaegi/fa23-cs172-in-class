8bdc5aa78f416984bca8ad48d4dd2f888da1c561
alain-lclark <alaink@lclark.edu>
2023-09-27 11:56:27 -0700

    refactor: Switch from a "for" to a "while" loop

--- minesweeperlib.py
+++ minesweeperlib.py
@@ -7,10 +7,14 @@ def count_neighboring_mines(minefield, x, y):
     """
     size = len(minefield)
     count = 0
-    for xx in range(x - 1, x + 2):
-        for yy in range(y - 1, y + 2):
+    xx = x - 1
+    while xx < x + 2:
+        yy = y - 1
+        while yy < y + 2:
             if (xx >= 0 and xx < size and yy >= 0 and yy < size):
                 (m, _) = minefield[xx][yy]
                 if m:
                     count += 1
+            yy += 1
+        xx += 1
     return count
