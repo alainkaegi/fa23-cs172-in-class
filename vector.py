f04a6a245e20b6dc6066fa3cdb5ed06bdc06d3a1
alain-lclark <alaink@lclark.edu>
2023-11-01 11:27:34 -0700

    feat: Add immutable Vectors (with defensive copies)

---
+++ vector.py
@@ -0,0 +1,16 @@
+class Vector:
+
+    def __init__(self, a):
+        self._coords = a[:]
+
+    def __str__(self):
+        s = '['
+        for i in range(len(self._coords) - 1):
+            s += str(self._coords[i]) + ', '
+        return s + str(self._coords[-1]) + ']'
+
+    def __add__(self, other):
+        r = Vector([0.0]*len(self._coords))
+        for i in range(len(self._coords)):
+            r._coords[i] = self._coords[i] + other._coords[i]
+        return r
