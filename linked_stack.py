20e50e877d4f02a8feb5fbce007a15cc1f5cadb3
alain-lclark <alaink@lclark.edu>
2023-11-29 11:23:04 -0800

    feat: Add support for equality testing and iteration

--- linked_stack.py
+++ linked_stack.py
@@ -13,6 +13,30 @@ class LinkedStack:
         self._top = self._top._next
         return item
 
+    def __eq__(self, other):
+        if self is other:
+            return True
+        selfn = self._top
+        othern = other._top
+        while not (selfn is None) and not (othern is None):
+            if selfn._item != othern._item:
+                return False
+            selfn = selfn._next
+            othern = othern._next
+        return selfn is None and othern is None
+
+    def __iter__(self):
+        self._iter_next_node = self._top
+        return self
+
+    def __next__(self):
+        if not (self._iter_next_node is None):
+            item = self._iter_next_node._item
+            self._iter_next_node = self._iter_next_node._next
+            return item
+        else:
+            raise StopIteration
+
     class Node:
         def __init__(self, item, next):
             self._item = item
