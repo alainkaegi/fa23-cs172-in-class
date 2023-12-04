ff85499c6d7abd02faac10dac8105356164ac3a9
alain-lclark <alaink@lclark.edu>
2023-12-04 10:58:43 -0800

    feat: Add is_empty and handle arbitrary size

--- array_stack.py
+++ array_stack.py
@@ -6,6 +6,8 @@ class ArrayStack:
         self._count = 0
 
     def push(self, item):
+        if self._count == len(self._data):
+            self._resize(len(self._data) * 2)
         self._data[self._count] = item
         self._count += 1
 
@@ -15,6 +17,11 @@ class ArrayStack:
         self._count -= 1
         return item
 
-
     def is_empty(self):
-        pass
+        return self.count == 0
+
+    def _resize(self, new_size):
+        new_data = Array(new_size)
+        for i in range(self._count):
+            new_data[i] = self._data[i]
+        self._data = new_data
